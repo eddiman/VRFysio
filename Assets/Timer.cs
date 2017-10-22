8d7ffdf2d856f9a06394cebacfdad5c73de7aa13
EddiStat <moledda@gmail.com>
2017-10-22 19:22:02 +0200

    added gaze input for displaying text with countdown

---
+++ Assets/Timer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text text;
+
+    float originalTimeLeft;
+    float timeLeft;
+    float timer;
+
+    bool isOnPoint = false;
+    bool finished;
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (isOnPoint)
+        {
+            finished = false;
+            timeLeft -= Time.deltaTime;
+            string timerString = timeLeft.ToString("0");
+           
+            text.text = "Hold for " + timerString + " seconds";
+
+            if(timeLeft <= 0)
+            {
+                Debug.Log("Time is up");
+                isOnPoint = false;
+                finished = true;
+                
+            }
+        } 
+
+    }
+
+    public void startTimer()
+    {
+        if (!isOnPoint)
+        {
+            isOnPoint = true;
+            Debug.Log("startTimer()");
+        }
+    }
+
+    public void setTimer(float timer)
+    {
+        if (!isOnPoint)
+        {
+            Debug.Log("setTimer()");
+            originalTimeLeft = timer;
+            timeLeft = timer;
+        }
+    }
+
+
+
+    public void stopTimer()
+    {
+        isOnPoint = false;
+        this.timeLeft = originalTimeLeft;
+
+        text.text = "Hold for " + originalTimeLeft.ToString() + " seconds";
+        if (finished)
+        {
+                text.text = "You did it!";
+
+        }
+
+    }
+
+
+
+}
---
+++ Assets/rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class rotation : MonoBehaviour {
+
+    public float speed = 0.1f;
+
+
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, 0.2f * Time.deltaTime);
+    }
+}
