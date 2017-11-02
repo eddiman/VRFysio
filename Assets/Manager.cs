58490f37dfccc2f1db3c9654e78aaec7f0c3f83f
eddiman <edbjorgen@gmail.com>
2017-11-02 18:03:07 +0100

    added missing files

---
+++ Assets/Manager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    protected Manager() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public static bool leftFinished = false;
+}
---
+++ Assets/MenuBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuBehavior : MonoBehaviour {
+
+    private float timer;
+
+    public float gazeTime = 1f;
+
+    public bool gazedAt;
+    private object Color;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+                timer = 0f;
+                Debug.Log("Timer is 0");
+
+            }
+        }
+    }
+
+    public void PointerEnter()
+    {
+        Debug.Log("Enter");
+        gazedAt = true;
+
+    }
+    public void PointerClick()
+    {
+
+        GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+
+
+        gazedAt = false;
+
+    }
+
+    public void PointerExit()
+    {
+        gazedAt = false;
+        timer = 0;
+    }
+}
